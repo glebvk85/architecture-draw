bd761fe0110cda1477ce6d4fe923b0d80742b228
gleb <g.kucherenko@corp.mail.ru>
2024-10-16 11:09:44 +0300

    extract data

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"log"
 	"os"
 	"strings"
@@ -15,9 +14,20 @@ type protoMethod struct {
 	MethodName    string
 }
 
+type linkInfo struct {
+	SourceServiceName string
+	TargetServiceName string
+	MethodName        string
+}
+
+type linkService struct {
+	SourceServiceName string
+	TargetServiceName string
+}
+
 func main() {
 	log.Println("started...")
-	projectPath := "C:\\Users\\g.kucherenko\\git\\server" //os.Args[1]
+	projectPath := "C:\\Users\\g.kucherenko\\git\\server\\Microservices" //os.Args[1]
 	log.Printf("scanning '%s'\n", projectPath)
 
 	protoChan := make(chan string)
@@ -28,11 +38,32 @@ func main() {
 	methods := parseProto(protoChan)
 
 	codeChan := make(chan string)
+	linkChan := make(chan linkInfo)
 	go func() {
 		defer close(codeChan)
 		scanDirectory(projectPath, ".cs", codeChan)
 	}()
-	parseCode(methods, codeChan)
+	go func() {
+		defer close(linkChan)
+		parseCode(methods, codeChan, linkChan)
+	}()
+
+	//links := make([]linkInfo, 0, 0)
+	serviceLinks := make(map[linkService]map[string]struct{}, 0)
+	services := make(map[string]struct{}, 0)
+	for v := range linkChan {
+		log.Printf("OUTPUT: %v\n", v)
+		key := linkService{v.SourceServiceName, v.TargetServiceName}
+		_, exists := serviceLinks[key]
+		if !exists {
+			serviceLinks[key] = make(map[string]struct{}, 0)
+
+		}
+		serviceLinks[key][v.MethodName] = struct{}{}
+		services[v.SourceServiceName] = struct{}{}
+		services[v.TargetServiceName] = struct{}{}
+	}
+	log.Println(services)
 }
 
 func scanDirectory(path string, ext string, output chan<- string) {
@@ -43,7 +74,7 @@ func scanDirectory(path string, ext string, output chan<- string) {
 	}
 	for _, entry := range entries {
 		if entry.IsDir() {
-			if entry.Name() == ".git" {
+			if entry.Name() == ".git" || strings.Contains(entry.Name(), ".FunctionalTests") || strings.Contains(entry.Name(), ".Tests") || strings.Contains(entry.Name(), "\\obj\\") {
 				continue
 			}
 			scanDirectory(path+"\\"+entry.Name(), ext, output)
@@ -57,7 +88,6 @@ func scanDirectory(path string, ext string, output chan<- string) {
 func parseProto(input <-chan string) []protoMethod {
 	r := make([]protoMethod, 0)
 	for v := range input {
-		log.Printf("parse proto %s\n", v)
 		parser := protoparse.Parser{}
 		files, err := parser.ParseFiles(v)
 		if err != nil {
@@ -71,7 +101,7 @@ func parseProto(input <-chan string) []protoMethod {
 			for _, s := range fdProto.GetService() {
 				for _, m := range s.Method {
 					log.Printf("Method: %s\n", m.GetName())
-					r = append(r, protoMethod{fdProto.GetPackage(), s.GetName(), m.GetName()})
+					r = append(r, protoMethod{fdProto.GetPackage(), s.GetName() + "Client", "." + m.GetName()})
 				}
 			}
 		}
@@ -79,21 +109,50 @@ func parseProto(input <-chan string) []protoMethod {
 	return r
 }
 
-func parseCode(methods []protoMethod, input <-chan string) {
+func parseCode(methods []protoMethod, input <-chan string, output chan<- linkInfo) {
 	for v := range input {
-		log.Printf("parse code %s\n", v)
-		f, err := os.Open(v)
+		//if !strings.Contains(v, "C:\\Users\\g.kucherenko\\git\\server\\Microservices\\ServerSearchService\\ServerSearchService.Daemon\\Providers\\GameServiceProvider.cs") {
+		//	continue
+		//}
+		b, err := os.ReadFile(v)
 		if err != nil {
 			log.Printf("Failed to open code file: %v\n", err)
-			continue
+		}
+		content := string(b)
+		//log.Println(content)
+		content = strings.ReplaceAll(content, "\r\n", " ")
+		content = strings.ReplaceAll(content, "\t", " ")
+		words := strings.Split(content, " ")
+
+		namespace := ""
+		clients := make(map[string]struct{}, 0)
+		for i, w := range words {
+			w := strings.Trim(w, ";")
+			if w == "namespace" {
+				namespace = strings.Trim(words[i+1], ";")
+			}
+			partWords := strings.Split(w, ".")
+			for _, pw := range partWords {
+				for _, m := range methods {
+					if m.ServiceName == pw {
+						clients[strings.Trim(words[i+1], ";")] = struct{}{}
+					}
+				}
+			}
 		}
 
-		s := bufio.NewScanner(f)
-		s.Split(bufio.ScanLines)
-		for s.Scan() {
-			line := s.Text()
-			//fmt.Println(fileScanner.Text())
+		for _, w := range words {
+			for c := range clients {
+				for _, m := range methods {
+					if strings.HasPrefix(w, c+m.MethodName+"(") || w == c+m.MethodName || strings.HasPrefix(w, c+m.MethodName+"Async(") || w == c+m.MethodName+"Async" {
+						output <- linkInfo{strings.Split(namespace, ".")[0], strings.Split(m.NamespaceName, ".")[1], m.MethodName}
+					}
+				}
+
+			}
 		}
-		f.Close()
+
+		//fmt.Printf("%s: %s\n", v, namespace)
+		//log.Printf("CLIENTS: %v\n", clients)
 	}
 }
